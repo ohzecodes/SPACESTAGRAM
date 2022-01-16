ed1da0685e0a395413e02ae1fde9bc619a13ffa0
ohzecodes <mo.ze@outlook.com>
2022-01-16 14:38:27 -0800

    all done

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'public/build'),
     },
     module: {
         rules: [
